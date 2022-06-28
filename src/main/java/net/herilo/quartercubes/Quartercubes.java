8c1da3a751e0dcaf61c51d35425fbdf0b3019708
Herilo <48388834+iiHerilo@users.noreply.github.com>
2022-06-28 10:28:17 -0500

    Config

--- src/main/java/net/herilo/quartercubes/Quartercubes.java
+++ src/main/java/net/herilo/quartercubes/Quartercubes.java
@@ -1,14 +1,14 @@
-package net.fabricmc.example;
+package net.herilo.quartercubes;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class Quartercubes implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	// That way, it's clear which mod wrote info, warnings, and er rors.
+	public static final Logger LOGGER = LoggerFactory.getLogger("quartercubes");
 
 	@Override
 	public void onInitialize() {
